21c6a1fd08078c9a50f957907ae24ab0f530e56d
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-09-28 13:14:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+// Created By: Evan
+// 
+// Created on: Sept. 2020
+// 
+// This program: shows temperature
+let current_temperature = input.temperature()
+basic.showString("" + (current_temperature))
+basic.showString("°C")
+basic.pause(1000)
+basic.clearScreen()
